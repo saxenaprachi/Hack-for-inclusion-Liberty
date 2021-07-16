b8a17c96a1d248c2e3dc84f3d2010b239cd2db88
Prachi Saxena <saxenaprachi963@gmail.com>
2021-07-16 12:10:58 +0530

    final

--- src/components/Dashboard/EmployeeDashboard.component.jsx
+++ src/components/Dashboard/EmployeeDashboard.component.jsx
@@ -81,7 +81,7 @@ export default function EmployeeDashboard() {
                                 <Menu.Item key={item}>
                                   {({ active }) => (
                                     <a
-                                      href="#"
+                                      href="/#"
                                       className={classNames(
                                         active ? 'bg-gray-100' : '',
                                         'block px-4 py-2 text-sm text-gray-700'
